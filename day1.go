61470baecde91c533259a978bf533ef976949aa8
zzeniou <zzeniou@apple.com>
2021-03-30 22:38:27 +0200

    Day 1 - Part 1

--- day1.go
+++ day1.go
@@ -1,5 +1,28 @@
 package main
 
+import "fmt"
+import "os"
+import "strconv"
+
+
 func Day1() (int, int) {
-	return 42,42
+
+	data := ReadLines("input/day1.txt")
+
+	total := 0
+
+	for _, weightStr := range data {
+		weight, err := strconv.Atoi(weightStr)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		total += calcFuel(weight)
+	}
+
+	return total,-1
+}
+
+func calcFuel(weight int) int {
+	return weight / 3 - 2
 }
