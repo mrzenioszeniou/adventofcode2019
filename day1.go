6469d9dccd8861042fb0ab4418a73e27c79f9939
zzeniou <zzeniou@apple.com>
2021-03-30 22:51:05 +0200

    Day 1 - Part 2

--- day1.go
+++ day1.go
@@ -7,20 +7,42 @@ import "strconv"
 
 func Day1() (int, int) {
 
-	data := ReadLines("input/day1.txt")
+	lines := ReadLines("input/day1.txt")
 
-	total := 0
-
-	for _, weightStr := range data {
+	data := []int {}
+	for _, weightStr := range lines {
 		weight, err := strconv.Atoi(weightStr)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		total += calcFuel(weight)
+		data = append(data, weight)		
 	}
 
-	return total,-1
+	day1 := 0
+	for _, weight := range data {
+		day1 += calcFuel(weight)
+	}
+
+	day2 := 0
+	for _, weight := range data {
+		last := calcFuel(weight)
+		fuel := last
+
+		for {
+			lastFuel := calcFuel(last)
+			if lastFuel < 0 {
+				break
+			} else {
+				fuel += lastFuel
+				last = lastFuel
+			}
+		}
+
+		day2 += fuel	
+	} 
+
+	return day1,day2
 }
 
 func calcFuel(weight int) int {
